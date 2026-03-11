281148174559bed8de16a34c43f150c7e81da7da
onurr_k <145716470+onurrrk@users.noreply.github.com>
2026-03-11 18:51:54 +0300

    Update and rename me/phantom/PhantomBlockerPlugin.java to src/main/java/me/phantom/mephantomPhantomBlockerPlugin.java

--- src/main/java/me/phantom/mephantomPhantomBlockerPlugin.java
+++ src/main/java/me/phantom/mephantomPhantomBlockerPlugin.java
@@ -7,11 +7,17 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PhantomBlockerPlugin extends JavaPlugin implements Listener {
+   
+   @Override
+   public void onEnable() {
+      getServer().getPluginManager().registerEvents(this, this);
+      getLogger().info("PhantomBlocker Activated!");
+   }
+
    @EventHandler
    public void onCreatureSpawn(CreatureSpawnEvent event) {
       if (event.getEntityType() == EntityType.PHANTOM) {
          event.setCancelled(true);
       }
-
    }
 }
